2c39705b4551d678fe8f00305b62410303d98fe7
MurtazaRaza-gitech <murtazaraza2286@gmail.com>
2025-12-31 18:26:50 +0500

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 
 import React from 'react';
 import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 const App: React.FC = () => {
   return (
